b68c4fdbe5ca6171d5104ebbba1a2fa6561ca131
Pankaj Puri <pankajpuri016@gmail.com>
2023-04-08 17:46:28 +1200

    Api designed for portfolio contact form

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ orgin: "http:localhost:8000/send-email" }));
+
+app.post("/send-email", (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "",
+      pass: "",
+    },
+  });
+
+  const mailOptions = {
+    from: "your-gmail-username@gmail.com",
+    to: "",
+    subject: "New message from your contact form",
+    html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send("Something went wrong");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
